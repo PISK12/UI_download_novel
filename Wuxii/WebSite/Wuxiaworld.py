91a11733f6f0a56be9d65c098f806d4526978224
Dell <piotr.k14@gmail.com>
2018-04-23 18:48:04 +0200

    wuxiaworl work,

--- Wuxii/WebSite/Wuxiaworld.py
+++ Wuxii/WebSite/Wuxiaworld.py
@@ -2,13 +2,14 @@ import sys
 sys.path.append("..")  # Adds higher directory to python modules path.
 
 from Wuxii.WebSiteParentsClass import BaseWebSite
-from Wuxii.Tools import Tools
+
+from icecream import ic
 
 
 class Wuxiaworld(BaseWebSite):
-    MAIN_ADRES = "http://www.wuxiaworld.com/"
-    OPTIONS_DIR = {"Completed": "menu-item-12207",
-                   "Chinese": "menu-item-2165", "Korean": "menu-item-116520"}
+    MAIN_ADRES = "http://www.wuxiaworld.com"
+    OPTIONS_DIR = {"Completed": "/tag/completed",
+                   "Chinese": "/language/chinese", "Korean": "/language/korean", "English": "/language/english"}
     NEXTCOMMAND = "Next Chapter"
 
     def __init__(self, class_ToFile):
@@ -17,64 +18,55 @@ class Wuxiaworld(BaseWebSite):
         self.list = list(self.OPTIONS_DIR.keys())
 
     def running(self):
-        adres = self._get_first_chapter(self.title, self.link)
-        while self.work and True != adres:
-            soup = self._download_www_to_text(adres)
-            if soup != None:
-                self.add_text_to_listWidget_from_Gui(adres)
-                adres = self._chack_and_get_next_adres(soup, adres)
-            else:
-                adres = True
+        ic()
 
-    def _chack_and_get_next_adres(self, soup, old_adres):
+        for adres in self._get_all_chapters(Wuxiaworld.MAIN_ADRES + self.link):
+            self.add_text_to_listWidget_from_Gui(adres)
+            self._download_www_to_text(Wuxiaworld.MAIN_ADRES + adres)
 
-        for link in soup.find_all('a')[:]:
-            if link.text == self.NEXTCOMMAND:
-                new_adres = link.get("href")
-                if new_adres != old_adres:
-                    return new_adres
-        return True
+    def _get_all_chapters(self, link):
+        ic()
+        soup = self.make_soup(link)
+        chapters = soup.find_all("li", {"class": "chapter-item"})
+        for chapter in chapters:
+            yield chapter.find("a").get("href")
 
-    def _get_first_chapter(self, title, adres, number=1):
-        self.getTitle(title)
-        soup = self.make_soup(adres)
-        for link in soup.find_all('a'):
-            if "chapter-" + str(number) in str(link.get('href')):
-                return link.get('href')
-            elif "book-" in str(link.get('href')):
-                return link.get('href')
-        raise "I can't find first chapter"
-
-    # download all titles with links
+    # get from website all titles with links
     def all_title_and_link_from_translating(self):
-        soup = self.make_soup(self.index_adres)
+        ic()
         dict_with_all_title_and_links = {}
-        for table in Wuxiaworld.OPTIONS_DIR:
-            for raw_link in soup.find(id=Wuxiaworld.OPTIONS_DIR[table]).find_all('a')[1:]:
-                text = raw_link.text
-                text = self.clean_from_bad_characters(text)
-                text = text.replace("()", "")
-                clear_link = raw_link.get('href')
-                dict_with_all_title_and_links[text] = clear_link
+        for link_type in Wuxiaworld.OPTIONS_DIR:
+            raw_link = "{}{}".format(
+                Wuxiaworld.MAIN_ADRES, Wuxiaworld.OPTIONS_DIR[link_type])
+            soup = self.make_soup(raw_link)
+            for part in soup.find_all('a'):
+                if "/novel/" in part.get('href'):
+                    if part.get('href').count("/") == 2:
+                        link = part.get('href').strip()
+                        title = part.text.strip()
+                        if title in dict_with_all_title_and_links:
+                            pass
+                        elif "" == title:
+                            pass
+                        else:
+                            dict_with_all_title_and_links[title] = link
         return dict_with_all_title_and_links
 
     def _download_www_to_text(self, adres):
-        name = adres.split("/")
-        if "index" in name[-2]:
-            name = name[-1]
-        else:
-            name = name[-2]
+        ic()
+        ic(adres)
+        name = adres.split("/")[-1]
         soup = self.make_soup(adres)
+        text = ""
         if soup != None:
-            soup = soup.find(itemprop="articleBody")
-            if soup != None:
-                text = soup.text
-                text = self._clean_big_text(text)
-                self.toTextFile(text, name)
-                return soup
+            soup = soup.find("div", {"class": "fr-view"})
+            for s in soup.find_all("p"):
+                text += s.text
+            self.toTextFile(text, name)
+
 
     def _clean_big_text(self, text):
-        text = text.replace("Previous Chapter", "").replace("Next Chapter", "")
+        ic()
         text = text.lstrip().rstrip()
         text = text.split("\r\n")
         texti = ""
--- Wuxii/WebSiteParentsClass.py
+++ Wuxii/WebSiteParentsClass.py
@@ -1,26 +1,22 @@
-import os
-import os.path
-from datetime import date
 from string import ascii_letters
 from sys import exc_info
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
-from zipfile import ZipFile
 
-from Wuxii.WriteOrReadFromFile import ToFile
-from Wuxii.Tools import Tools
-
+import requests
 from PyQt5.QtCore import QThread
 from bs4 import BeautifulSoup
-import requests
+from icecream import ic
 
 
 class BaseWebSite(QThread):
-    MAIN_ADRES=""
+    MAIN_ADRES = ""
 
     def __init__(self, class_ToFile):
+        ic.enable()
+        ic.disable()
         super(QThread, self).__init__()
-        self.cacheCookies={}
+        self.cacheCookies = {}
         self.class_ToFile = class_ToFile
         self.toTextFile = self.class_ToFile.toTextFile
         self.title = ""
@@ -28,10 +24,12 @@ class BaseWebSite(QThread):
         self.work = True
 
     def set_title_and_link(self, title, link):
+        ic()
         self.title = title
         self.link = link
 
     def run(self):
+        ic()
         self.work = True
         self.swith_button_from_GUI()
         self.running()
@@ -42,6 +40,7 @@ class BaseWebSite(QThread):
         pass
 
     def getTitle(self, tite_novel):
+        ic()
         self.title = tite_novel
         self.title = self.clean_from_bad_characters(
             self.title).replace(" ", "_")
@@ -49,14 +48,19 @@ class BaseWebSite(QThread):
             self.title = self.title[:-1]
 
     def make_soup(self, url):
+        ic()
         try:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             webpage = urlopen(req).read()
             return BeautifulSoup(webpage, "html.parser")
-        except HTTPError:
-            print("it's last chapter")
+        except HTTPError as error:
+            ic()
+            ic(error)
+            ic(url)
         except:
-            print(exc_info())
+            ic()
+            ic(exc_info())
+            ic(url)
             exit()
 
     def clean_to_name_file(self, text):
@@ -67,6 +71,7 @@ class BaseWebSite(QThread):
         return new
 
     def clean_from_bad_characters(self, text):
+        ic()
         new = ""
         for i in text:
             if i.isdigit() or i in ascii_letters or i == " ":
@@ -78,28 +83,46 @@ class BaseWebSite(QThread):
         return new
 
     def swith_button_from_GUI(self):
+        ic()
         if self.button.isEnabled():
             self.button.setEnabled(False)
         else:
             self.button.setEnabled(True)
 
     def add_text_to_listWidget_from_Gui(self, text):
-        self.listWidget.insertItem(0, text)
+        try:
+            ic()
+            self.listWidget.insertItem(0, text)
+        except AttributeError as e:
+            ic()
+        except Exception as e:
+            ic()
+            ic(e)
 
     def button_from_GUI(self, button):
-        self.button = button
+        try:
+            ic()
+            self.button = button
+        except AttributeError as e:
+            ic()
+        except Exception as e:
+            ic()
+            ic(e)
 
     def listWidget_from_Gui(self, listWidget):
+        ic()
         self.listWidget = listWidget
 
     def stop(self):
+        ic()
         self.work = False
 
-    def getCookies(self,adres="")->dict:
+    def getCookies(self, adres="") -> dict:
+        ic()
         if adres in self.cacheCookies:
             pass
         else:
             session = requests.Session()
             response = session.get(adres)
-            self.cacheCookies[adres]=session.cookies.get_dict()
+            self.cacheCookies[adres] = session.cookies.get_dict()
         return self.cacheCookies[adres]
